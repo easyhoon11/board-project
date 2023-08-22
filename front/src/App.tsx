b6eb48e28e02f2c56cf1a1984675cd1d9a4afb3a
easy <leege5664@gmail.com>
2023-08-22 13:46:22 +0900

    20230822

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -8,3 +8,12 @@ function App() {
 }
 
 export default App;
+
+// ! 네비게이션 설계
+// ! 메인 화면 : '/' - Main
+// ! 로그인 화면 + 회원가입 화면 : '/auth' - Authentication
+// ! 검색 화면 : '/search/:word' - Search
+// ! 게시물 상세 보기 화면 : '/board/detail/:boardNumber' - BoardDetail
+// ! 게시물 작성 화면 : '/board/write' - BoardWrite
+// ! 게시물 수정 화면 : '/board/update/:boardNumber' - BoardUpdate
+// ! 유저 게시물 화면 : '/user/:email' - User
