c544812dd19b09b75858cb827f6ff10c7a51a91f
easy <leege5664@gmail.com>
2023-08-23 13:51:08 +0900

    20230823

---
+++ front/src/components/InputBox/index.tsx
@@ -0,0 +1,40 @@
+import React from "react";
+import "./style.css";
+
+//          interface: Input 상자 컴포넌트 Props          //
+interface Props {
+  label: string;
+  type: "text" | "password" | "email";
+  placeholder: string;
+  icon?: string;
+  errorMessage?: string;
+  onButtonClick?: () => void;
+}
+
+//          component: input 상자 컴포넌트          //
+export default function InputBox(props: Props) {
+  //          state: Properties         //
+  const { label, type, placeholder, icon, errorMessage } = props;
+  const { onButtonClick } = props;
+
+  //          render: input 상자 렌더링          //
+  return (
+    <div className="inputbox">
+      <div className="inputbox-label">{label}</div>
+      <div
+        className={
+          errorMessage ? "inputbox-container-error" : "inputbox-container"
+        }
+      >
+        <input className="input" type={type} placeholder={placeholder} />
+        {onButtonClick !== undefined && (
+          <div className="icon-button" onClick={onButtonClick}>
+            {icon}
+            <div className="right-arrow-icon"></div>
+          </div>
+        )}
+      </div>
+      <div className="inputbox-message">{errorMessage}</div>
+    </div>
+  );
+}
